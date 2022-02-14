953d804222c19610972f098e0da0efad01383e67
yqlbu <yqlbu@bu.edu>
2022-02-14 22:05:00 +0800

    fix: updated configurations

--- packer-templates/scripts/bootstrap.sh
+++ packer-templates/scripts/bootstrap.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Install MC Client
-echo '==> Installing MC Client ...'
+echo 'Installing MC Client ...'
 wget -q https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x mc
 sudo mv mc /usr/bin
-mc --autocompletion
+mc --autocompletion >/dev/null 2>&1
 
 # Cleans Audit Log
-echo '==> Cleaning all audit logs ...'
+echo 'Cleaning all audit logs ...'
 [[ -f /var/log/audit/audit.log ]] && sudo cat /dev/null | sudo tee /var/log/audit/audit.log
 [[ -f /var/log/wtmp ]] && sudo cat /dev/null | sudo tee /var/log/wtmp
 [[ -f /var/log/lastlog ]] && sudo cat /dev/null | sudo tee /var/log/lastlog
 
 # Clean apt-get.
-echo '==> Cleaning apt-get ...'
-sudo apt-get clean
+echo 'Cleaning apt cache ...'
+sudo apt-get autoclean
 
 # Modifying ssh configuration
 echo '==> Modifying ssh configuration ...'
@@ -23,19 +23,10 @@ sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo service sshd restart
 
-# Sets hostname to localhost.
-echo '==> Setting hostname to localhost ...'
-sudo cat /dev/null | sudo tee /etc/hostname
-sudo hostnamectl set-hostname localhost
-
-# Add dbus service
-echo '==> Adding dbus service ...'
-sudo awk -i inplace '1;/After=systemd-remount-fs.service/{print "After=dbus.service"}' /lib/systemd/system/open-vm-tools.service
-
 # Cleans the machine-id.
-echo '==> Cleaning the machine-id ...'
+echo 'Cleaning the machine-id ...'
 sudo truncate -s 0 /etc/machine-id
 sudo rm /var/lib/dbus/machine-id
 sudo ln -s /etc/machine-id /var/lib/dbus/machine-id
 
-echo '==> Provisioning finished, enjoyed!'
+echo 'Provisioning finished, enjoyed!'
