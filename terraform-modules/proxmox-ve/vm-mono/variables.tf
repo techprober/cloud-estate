01ba922ce3a27342f7631faf3cf1bc19f9983457
yqlbu <yqlbu@bu.edu>
2022-03-19 11:39:30 +0800

    feat: introduced VLAN tag

--- terraform-modules/proxmox-ve/vm-mono/variables.tf
+++ terraform-modules/proxmox-ve/vm-mono/variables.tf
@@ -116,6 +116,12 @@ variable "vm_network_bridge" {
   type        = string
 }
 
+variable "vm_network_tag" {
+  default     = -1
+  description = "the VLAN tag to apply to packets on this device. By default -1 disables VLAN tagging."
+  type        = number
+}
+
 variable "vm_network_ip" {
   description = "default vm network ip e.g 192.168.100"
   type        = string
