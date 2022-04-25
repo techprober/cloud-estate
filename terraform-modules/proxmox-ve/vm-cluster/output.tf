6cf123fb2f5646be4c06006ed11d091266eaf27e
yqlbu <yqlbu@bu.edu>
2022-04-25 19:07:36 +0800

    fix: fixed ipv4 naming reference

--- terraform-modules/proxmox-ve/vm-cluster/output.tf
+++ terraform-modules/proxmox-ve/vm-cluster/output.tf
@@ -1,13 +1,13 @@
 output "master_ipv4_static_ip" {
   value = {
     for vm in proxmox_vm_qemu.master_node :
-    vm.name => vm.default_ip_address
+    vm.name => vm.default_ipv4_address
   }
 }
 
 output "worker_ipv4_static_ip" {
   value = {
     for vm in proxmox_vm_qemu.worker_node :
-    vm.name => vm.default_ip_address
+    vm.name => vm.default_ipv4_address
   }
 }
