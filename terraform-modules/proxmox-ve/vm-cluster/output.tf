7f2294e3cc5f8a2c316578ac352b4162b12fa5ce
yqlbu <yqlbu@bu.edu>
2022-04-25 19:04:58 +0800

    fix: fixed typos

--- terraform-modules/proxmox-ve/vm-cluster/output.tf
+++ terraform-modules/proxmox-ve/vm-cluster/output.tf
@@ -1,13 +1,13 @@
 output "master_ipv4_static_ip" {
   value = {
-    for vm in prxmox_vm_qemu.master_node :
+    for vm in proxmox_vm_qemu.master_node :
     vm.name => vm.default_ip_address
   }
 }
 
 output "worker_ipv4_static_ip" {
   value = {
-    for vm in prxmox_vm_qemu.worker_node :
+    for vm in proxmox_vm_qemu.worker_node :
     vm.name => vm.default_ip_address
   }
 }
