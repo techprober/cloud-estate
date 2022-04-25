5c81c0aae1572dbc977dcf12e27d3c65381e99d1
yqlbu <yqlbu@bu.edu>
2022-04-25 19:02:10 +0800

    fix: fixed naming convention

--- terraform-modules/proxmox-ve/vm-cluster/output.tf
+++ terraform-modules/proxmox-ve/vm-cluster/output.tf
@@ -1,11 +1,11 @@
-output "master_node(s)_ipv4_static_ip" {
+output "master_ipv4_static_ip" {
   value = {
     for vm in prxmox_vm_qemu.master-node :
     vm.name => vm.default_ip_address
   }
 }
 
-output "worker_node(s)_ipv4_static_ip" {
+output "worker_ipv4_static_ip" {
   value = {
     for vm in prxmox_vm_qemu.worker-node :
     vm.name => vm.default_ip_address
